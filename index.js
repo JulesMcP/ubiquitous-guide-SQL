5ab0924b16951ba875222d580a8b60a3fdf687aa
JulesMcP <rogue427@yahoo.com>
2022-03-13 19:43:59 -0500

    prompt department function added

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ connection.connect(function (err) {
             break;
         }
       });
-  }
+}
   
   //View Employees
 
@@ -84,7 +84,8 @@ function viewEmployee() {
       firstPrompt();
     });
   
-  }
+}
+
 //View Employees by Department
 
 //array
@@ -114,4 +115,38 @@ function viewEmployeeByDepartment() {
       promptDepartment(departmentChoices);
     });
   
+}
+
+function promptDepartment(departmentChoices) {
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Which department would you choose?",
+          choices: departmentChoices
+        }
+      ])
+      .then(function (answer) {
+        console.log("answer ", answer.departmentId);
+  
+        var query =
+          `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department 
+    FROM employee e
+    JOIN role r
+      ON e.role_id = r.id
+    JOIN department d
+    ON d.id = r.department_id
+    WHERE d.id = ?`
+  
+        connection.query(query, answer.departmentId, function (err, res) {
+          if (err) throw err;
+  
+          console.table("response ", res);
+          console.log(res.affectedRows + "Employees are viewed!\n");
+  
+          firstPrompt();
+        });
+      });
   }
