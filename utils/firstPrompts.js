f9d02cc91b3255337a6d86cfc104de1237f404f7
JulesMcP <rogue427@yahoo.com>
2022-05-09 18:57:04 -0500

    adds firstPrompt code

---
+++ utils/firstPrompts.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+
+const firstPrompts = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "main",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add A Department",
+                "Add A Role",
+                "Add A New Employee",
+                "Update An Employee Role",
+                "Exit",
+            ],
+        },
+    ]);
+};
+
+module.exports = firstPrompts;
