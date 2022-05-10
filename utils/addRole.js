5fd6ecece263ce2592579b6ea5708ce9cda1941a
JulesMcP <rogue427@yahoo.com>
2022-05-09 20:36:40 -0500

    adds code to addRole file

---
+++ utils/addRole.js
@@ -0,0 +1,40 @@
+const inquirer = require("inquirer");
+
+const addRole = async (departments) => {
+    departments = departments.map((x) => x.name);
+    return inquirer.prompt([
+        {   
+            type: "input",
+            name: "role",
+            message: "Enter the name of the role.",
+            validate: (role) => {
+                if (role) {
+                    return true;
+                } else {
+                    console.log("\n You must enter a role name.");
+                    return false;
+                }
+            },
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "Enter the salary for this role.",
+            validate: (salary) => {
+                if (isNaN(salary) === false) {
+                    return true;
+            } else {
+                console.log("\n Please enter a number.");
+                return false;
+            }
+            },
+        },
+        {
+            type: "input",
+            name: "whichDepartment",
+            message: "What department does this role belong to?",
+        },
+    ]);
+};
+
+module.exports = addRole;
